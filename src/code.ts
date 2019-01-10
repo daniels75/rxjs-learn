48d2f0da26bd16252f07dcb7e482686b187e6abd
Daniels <daniel.sadowski75@gmail.com>
2019-01-10 23:28:19 +0100

    Step 1.0.1 of Example

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,10 @@
 import { Subject, Observable } from 'rxjs';
+import {of} from 'rxjs';
 
-console.log(Observable);
+var obser: Observable<any> = new Observable<any>();
+
+var obsFrom  = of(1,2,3);
+
+obsFrom.subscribe( {
+    next: value => console.log(`Current value: ${value}`)
+})
